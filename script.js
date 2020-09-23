1dc910fc7fe1dbce07446394c746f5f994923652
VladZach <vladzach25@gmail.com>
2020-09-24 00:11:01 +0300

    Completed script and fixed styles

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let keys = document.querySelectorAll('.key')
-document.addEventListener('keydown', playSound)
-keys.forEach(item => item.addEventListener('keydown', playSound))
 function playSound(e) {
-  console.log(e)
   if(event.repeat) return
   let key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
   if (!key) return
+  key.classList.add('playing')
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   audio.currentTime = 0
   audio.play()
 }
+
+function transformBack(e) {
+  if (e.propertyName != 'transform') return
+  e.target.classList.remove('playing')
+}
+
+let keys = document.querySelectorAll('.key')
+document.addEventListener('keydown', playSound)
+keys.forEach(item => item.addEventListener('transitionend', transformBack))
